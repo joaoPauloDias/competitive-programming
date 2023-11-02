123e20b02624a7bd669c12b669cfbf3d7f76f225
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2023-11-02 01:25:21 -0300

    adicionando B - JZZhu and Cities

--- cf_isolated/jzzhuandcities.cpp
+++ cf_isolated/jzzhuandcities.cpp
@@ -1,48 +1,70 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 using ll = long long;
-const ll INF = 1e17;
-using pii = pair<ll, ll>;
-using tiii = pair<ll,ll,ll>;
+using pii = pair < int, int > ;
+using tiii = tuple < ll, ll, int > ;
 
-int main(){
-    int n,m,k;
-    cin>>n>>m>>k;
-    vector<vector<tiii>> adj(n+5, vector<tiii>());
+const ll INF = 1e18;
+const int MAXN = 1e5 + 50;
 
-    while(m--){
+bool used[MAXN];
+int total[MAXN];
+int ans;
+
+vector < vector < tiii >> adj(MAXN, vector < tiii > ());
+priority_queue < tiii, vector < tiii > , greater < tiii >> q;
+vector < ll > d(MAXN, INF);
+vector < pii > r;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    while (m--) {
         int u, v, x;
-        cin>>u>>v>>x;
+        cin >> u >> v >> x;
         adj[u].emplace_back(v, x, 0);
         adj[v].emplace_back(u, x, 0);
     }
 
-    while(k--){
+    while (k--) {
         int s, y;
-        cin>>s>>y;
+        cin >> s >> y;
         adj[1].emplace_back(s, y, 1);
         adj[s].emplace_back(1, y, 1);
+        r.emplace_back(s, y);
     }
 
-    priority_queue<tiii, vector<tiii>, greater<tiii>> q;
-    vector<vector<ll>> d(n+5, vector<ll> (2, INF));
+    q.emplace(0, 0, 1);
 
-    d[1][0] = d[1][1] = 0;
-
-    q.emplace(0, 1, 0);
-    q.emplace(0, 1, 1);
-
-    while(!q.empty()){
-        auto [d_v, v, t_v] = q.top();
+    while (!q.empty()) {
+        auto[d_v, t_v, v] = q.top();
         q.pop();
-        if(d_v>d[v][t_v])
+        if (INF != d[v])
             continue;
-        for(auto [u, len, t_u]:adj[v]){
-            if(d[u]>d_v + len){
-                d[u] = 
+        used[v] = t_v;
+        d[v] = d_v;
+        for (auto[u, len, t_u]: adj[v]) {
+            ll d_u = d_v + len;
+            if (d_u < d[u]) {
+                q.emplace(d_u, t_u, u);
             }
         }
     }
 
+    for (auto[s, y]: r) {
+        if (y > d[s]) ans++;
+        else {
+            if (used[s] == 0) ans++;
+            else ans+=(++total[s])>1;
+        }
+    }
+    
+    cout << ans << endl;
 }
